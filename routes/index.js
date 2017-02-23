4662ed737f16c30205ffdd3cdadb7bd23d77facf
Jesse L.K. Overton <jesse@ovrtn.com>
2017-02-22 19:46:47 -0500

    Removed API from repo

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 // app/routes
 
 module.exports = function(router) {
-    require('./api')(router.api);
+    require('./atlas')(router.atlas);
     require('./main')(router.main);
 };
--- server.js
+++ server.js
@@ -5,10 +5,8 @@ var dotenv = require('dotenv').load();
 var subdomain = require('express-subdomain');
 var express = require('express');
 // var passport = require('passport');
-var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 // var methodOverride = require('method-override');
-var mongoose = require('mongoose');
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -21,10 +19,6 @@ var app = express();
 var port = (process.env.PORT || 8080);
 
 app.disable('x-powered-by');
-
-var database = require('./config/database');
-mongoose.connect(database.url);
-//
 //require('./config/passport')(passport);
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -33,46 +27,20 @@ app.use(bodyParser.json({limit: "100mb"}));
 
 
 
-// CONNECTION EVENTS
-// When successfully connected
-mongoose.connection.on('connected', function () {
-  console.log('Mongoose default connection open');
-});
-
-// If the connection throws an error
-mongoose.connection.on('error',function (err) {
-  console.log('Mongoose default connection error: ' + err);
-});
-
-// When the connection is disconnected
-mongoose.connection.on('disconnected', function () {
-  console.log('Mongoose default connection disconnected');
-});
-
-// If the Node process ends, close the Mongoose connection
-process.on('SIGINT', function() {
-  mongoose.connection.close(function () {
-    console.log('Mongoose default connection disconnected through app termination');
-    process.exit(0);
-  });
-});
-
-
-
 // Passport Configuration ================================================================
 // app.use(passport.initialize());
 
 // Routes ================================================================================
 var routers = {};
-routers.api = express.Router();
+routers.atlas = express.Router();
 routers.main = express.Router();
 
 require('./routes')(routers);
 
 if (process.env.NODE_ENV == 'development') {
-    app.use('/api', routers.api);
+    app.use('/atlas', routers.atlas);
 } else {
-    app.use(subdomain('api', routers.api));
+    app.use(subdomain('atlas', routers.atlas));
 }
 
 app.use('/', routers.main);
